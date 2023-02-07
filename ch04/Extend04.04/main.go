56b3209d0953c99eed1906bd815ef39ceedaeb9f
Fu-Ming <716585+sary357@users.noreply.github.com>
2023-02-07 09:59:35 +0800

    updated

--- ch04/Extend04.04/main.go
+++ ch04/Extend04.04/main.go
@@ -16,20 +16,19 @@ func getUser(id string) (string, bool) {
 }
 
 func main() {
-	for {
-		if len(os.Args) < 2 {
-			fmt.Println("No valid ID. Please ensure you already input ID")
-			os.Exit(1)
-		}
-		for i := 1; i < len(os.Args); i++ {
-			//fmt.Println(os.Args[i])
-			userName, isExisted := getUser(os.Args[i])
-			if isExisted {
-				fmt.Printf("Hello, %s (%v)\n", userName, os.Args[i])
-			} else {
-				fmt.Printf("No user id (%v)\n", os.Args[i])
-			}
-		}
-		os.Exit(0)
+
+	if len(os.Args) < 2 {
+		fmt.Println("No valid ID. Please ensure you already input ID")
+		os.Exit(1)
 	}
+	for i := 1; i < len(os.Args); i++ {
+		//fmt.Println(os.Args[i])
+		userName, isExisted := getUser(os.Args[i])
+		if isExisted {
+			fmt.Printf("Hello, %s (%v)\n", userName, os.Args[i])
+		} else {
+			fmt.Printf("No user id (%v)\n", os.Args[i])
+		}
+	}
+
 }
