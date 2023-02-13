4d5d94dd1d48878e0d4af594317c742acd21203f
Fu-Ming <716585+sary357@users.noreply.github.com>
2023-02-13 09:49:16 +0800

    updated

---
+++ ch05/Extend05.01/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Developer struct {
+	Name       string
+	HourlyRate uint
+	WorkWeek   [7]uint8
+}
+
+type Weekday uint8
+
+const (
+	Sunday Weekday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
+
+func (d *Developer) logHours(day Weekday, hours uint8) {
+	d.WorkWeek[day] = hours
+}
+
+func (d Developer) WeekPayment() uint {
+	var r uint = 0
+	for _, v := range d.WorkWeek {
+		r = d.HourlyRate*uint(v) + r
+	}
+	return r
+}
+
+func main() {
+	developer := Developer{}
+	developer.HourlyRate = 10
+	developer.WorkWeek[Sunday] = 0
+	developer.WorkWeek[Monday] = 8
+	developer.WorkWeek[Tuesday] = 10
+	fmt.Printf("Working hours on Monday: %d.\n", developer.WorkWeek[Sunday])
+	fmt.Printf("Working hours on Tuesday: %d.\n", developer.WorkWeek[Tuesday])
+	fmt.Printf("Weekly Wage: %d\n", developer.WeekPayment())
+}
