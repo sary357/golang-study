b924b2cc9fb2d192ab35f06bfc8a9458f3d14b17
Fu-Ming <716585+sary357@users.noreply.github.com>
2023-03-27 12:17:10 +0800

    updated

--- ch10/Extend10.01/main.go
+++ ch10/Extend10.01/main.go
@@ -18,7 +18,7 @@ func deadline(t time.Time, tz string) (time.Time, error) {
 func main() {
 	testDate := time.Date(2021, 4, 23, 16, 18, 15, 1, time.Local)
 	deadlineTime, err := deadline(testDate, "Asia/Tokyo")
-	if err == nil {
+	if err == nil { //15: 24 小時制, 04: 2 digits表示 "時", 05: 2 digits 表示"分", 01: 2 digits 表示"月", 02: 2 digits 表示"日", 2006: 4 位數表示"年"
 		fmt.Println("curret time (in Taipei time)\t: " + testDate.Format("15:04:05 01/02/2006"))
 		fmt.Println("deadline (in Tokyo time)\t: " + deadlineTime.Format("15:04:05 01/02/2006"))
 	} else {
