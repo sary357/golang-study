7bdce799db7b59215c3e2ff2874776ca24285105
Fu-Ming <716585+sary357@users.noreply.github.com>
2023-03-29 08:37:01 +0800

    updated

---
+++ ch10/Extend10.02/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestMain(t *testing.T) {
+	loop := 1000
+	for i := 0; i < loop; i++ {
+		start := time.Now()
+		value := Fibonacci(30)
+		if value != 832040 {
+			t.Fatal("Expected result: 832040. The value we get is: " + strconv.Itoa(value))
+		}
+		end := time.Now()
+		duration := end.Sub(start)
+		fmt.Println(time.Now().Format("2006/01/02 15:04:05")+" ", duration)
+	}
+}
